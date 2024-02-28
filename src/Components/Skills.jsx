daaf57d35bb8a377cbb9fea94f117f7eddda3e20
uzair-bmj <uzairbmj@gmail.com>
2024-02-28 22:52:22 +0500

    complete except projects

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import "./Skills.css"
 
-export default function Logos() {
+export default function Skills() {
   return (
     <>
       <div className='w-full bg-gray-400  relative' id='skills'>
-        <img src="/bg2.jpg" alt=""  className=' opacity-5 h-[40vh] w-full absolute'/>
-        <div className='flex justify-center flex-wrap w-full py-10'>
-        <img src="/htm.png" alt="" className='w-[100px] img'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        <img src="/htm.png" alt="" className='w-[100px]'/>
-        
-
+        <img src="/bg2.jpg" alt="" className=' opacity-5 h-[40vh] w-full absolute' />
+        <div className='flex justify-between flex-wrap w-[80vw] m-auto py-10 px-10'>
+          <abbr title="HTML5"> <i class="fa-brands fa-html5 sk"></i></abbr>
+          <abbr title="CSS3"><i class="fa-brands fa-css3-alt sk"></i></abbr>
+          <abbr title="BOOTSTRAP 5"><i class="fa-brands fa-bootstrap sk"></i></abbr>
+          <abbr title="JAVSCRIPT"><i class="fa-brands fa-js sk"></i></abbr>
+          <abbr title="REACT"><i class="fa-brands fa-react sk"></i></abbr>
+          <abbr title="NODE JS"><i class="fa-brands fa-node sk"></i></abbr>
+          <abbr title="GIT"><i class="fa-brands fa-github sk"></i></abbr>
         </div>
 
       </div>
